93d8bb057c142492e036d5ef91e76c9074c70d95
ppmasa8 <kiyokiyopip56@gmail.com>
2022-03-06 01:12:41 +0900

    Added textController and Text

--- lib/view/utils/parts/textform.dart
+++ lib/view/utils/parts/textform.dart
@@ -9,31 +9,47 @@ class TextForm extends StatefulWidget {
 }
 
 class _TextFormState extends State<TextForm> {
+  final textController = TextEditingController();
+  String plainText = '';
   String text = '';
+
+  void _updateText() {
+    setState(() {
+      plainText = textController.text;
+      print(plainText);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       padding: const EdgeInsets.all(60.0),
       child: Column(
         children: [
+          Text(
+            plainText,
+          ),
           TextField(
             maxLength: 5,
-            inputFormatters: <TextInputFormatter> [
+            inputFormatters: <TextInputFormatter>[
               FilteringTextInputFormatter.allow(RegExp(r'^[\u3040-\u309F]+$')),
             ],
+            controller: textController,
             decoration: const InputDecoration(
               border: OutlineInputBorder(),
               labelText: 'ひらがなで文字を入力してね',
             ),
             onChanged: (letter) {
               text = letter;
+              print(text);
             },
+          ),
+          TextButton(
+            child: const Text('Submit'),
+            onPressed: _updateText,
           ),
         ],
       ),
     );
   }
 }
-
-
-
